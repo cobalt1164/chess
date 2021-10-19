771c061d3792a76f7cd8bbcb1af8b73229322b2e
cobalt1164 <xcobalt10101@gmail.com>
2021-10-19 13:06:20 -0500

    Add FEN parsing to board.c

--- src/board.c
+++ src/board.c
@@ -1,5 +1,7 @@
-#include "stdio.h"
+#include "stdio.h" 
 #include "types.h"
+#include "assert.h"
+
 
 void resetBoard(BOARD_STRUCT *board) {
     int i = 0;
@@ -36,4 +38,106 @@ void resetBoard(BOARD_STRUCT *board) {
     board->positionKey = 0ULL;
 
 
+}
+
+int parseFen(char *fen, BOARD_STRUCT *board) {
+    assert(fen != NULL);
+    assert(board != NULL);
+
+    int rank = RANK_8;
+    int file = FILE_A;
+    int piece = 0;
+    int count = 0;
+    int i = 0;
+    int square64 = 0;
+    int square120 = 0;
+
+    resetBoard(board);
+    
+    while ((rank >= RANK_1) && *fen) {
+        count = 1;
+        switch (*fen) {
+            case 'p': piece=BLACK_PAWN; break;
+            case 'r': piece=BLACK_ROOK; break;
+            case 'n': piece=BLACK_KNIGHT; break;
+            case 'b': piece=BLACK_BISHOP; break;
+            case 'k': piece=BLACK_KING; break;
+            case 'q': piece=BLACK_QUEEN; break;
+            case 'P': piece=WHITE_PAWN; break;
+            case 'R': piece=WHITE_ROOK; break;
+            case 'N': piece=WHITE_KNIGHT; break;
+            case 'B': piece=WHITE_BISHOP; break;
+            case 'K': piece=WHITE_KING; break;
+            case 'Q': piece=WHITE_QUEEN; break;
+
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+                      piece = EMPTY;
+                      count = *fen - '0';
+                      break;
+
+            case '/': case ' ':
+                      rank--;
+                      file = FILE_A;
+                      fen++;
+                      continue;
+
+            default:
+                      printf("Error in FEN parse\n");
+                      return -1;
+        }
+
+        for (i = 0; i < count; ++i) {
+            if (piece != EMPTY) {
+                board->pieces[fileRankTo120(file, rank)] = piece;
+            }
+            file++;
+        }
+        fen++;
+    }
+
+    assert(*fen == 'w' || *fen == 'b');
+
+    board->side = (*fen == 'w') ? WHITE : BLACK;
+    fen += 2;
+
+    for (i = 0; i < 4; ++i) {
+        if (*fen == ' ') {
+            break;
+        }
+
+        switch (*fen) {
+            case 'K': board->castlePerms |= WHITE_KING_CASTLING; break;
+            case 'Q': board->castlePerms |= WHITE_QUEEN_CASTLING; break;
+            case 'k': board->castlePerms |= BLACK_KING_CASTLING; break;
+            case 'q': board->castlePerms |= BLACK_QUEEN_CASTLING; break;
+            default:
+                      break;
+        }
+        fen++;
+    }
+
+    fen++;
+
+    assert(board->castlePerms >= 0 && board->castlePerms <= 15);
+    
+    if (*fen != '-') {
+        // Simple way to convert file/rank string to int
+        file = fen[0] - 'a';
+        rank = fen[1] - '1';
+        assert(file >= FILE_A && file <= FILE_H);
+        assert(rank >= RANK_1 && rank <= RANK_8);
+
+        board->enPassant = fileRankTo120(file, rank);
+    }
+
+    board->positionKey = genPositionKey(board);
+
+    return 0;
 }
